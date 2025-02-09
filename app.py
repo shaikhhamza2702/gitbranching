280170eb6a429dd8bd1287375d54d3ec47d0cff4
Hamzah shaikh <135532413+shaikhhamza2702@users.noreply.github.com>
2025-02-09 21:19:44 +0530

    Create app.py
    first commit from github

---
+++ app.py
@@ -0,0 +1,16 @@
+# Sample Python Code
+
+# Define a function that calculates the area of a rectangle
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# Assign values to length and width
+length = 5
+width = 3
+
+# Call the function with arguments
+area = calculate_area(length, width)
+
+# Print the result
+print(f"The area of the rectangle with length {length} and width {width} is {area}.")
