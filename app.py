07a7f19e833774bab253eaed10cb32a66ab9d14b
Hamzah shaikh <135532413+shaikhhamza2702@users.noreply.github.com>
2025-02-09 23:42:20 +0530

    Update app.py
    7github

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 
 # Define a function that calculates the area of a rectangle
 def calculate_area(length, width):
-   total area of rectangle= length * width
+   total area of rectangle= length * width *(-1-1)
     return area
-#this is the 5th edit from github ,here we will try with same line changes"
+#this is the 6th edit from github ,here we will try with same line changes"
 
 # Assign values to length and width
 length = 5.0
